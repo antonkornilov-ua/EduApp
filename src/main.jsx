8d03be1dfd5eed61a881246d6d0eeb271fb45d73
antonkornilov-ua <anton.kornilov@hotmail.com>
2023-04-12 17:04:02 +0300

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import { routing } from './routes/Routing'
+import routing  from './routes/Routing'
 
 
 
